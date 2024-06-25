fe619eb3527a0c914f467321dab18c7dae847de8
Alex_Pasini <alexantoiopasini20@gmail.com>
2024-06-25 20:12:14 -0300

    aula_01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {path:"",pathMatch:'full',redirectTo:'home'},
+
+    {
+        path:'',
+        component:LayoutComponent,
+        children: [
+            {
+                path:"home",
+                component: HomeComponent
+            }
+        ]    
+    }
+
+];
